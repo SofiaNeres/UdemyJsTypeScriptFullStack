57bdb6fbac3123bd9160df5d2a883d368955aafa
Sofia Neres <contatosofianeres@gmail.com>
2024-07-08 18:24:19 -0300

    Realizando exercício de alert, confirm e prompt

---
+++ curso-js-ts/js-basico/alert-confirm-prompt/js/index.js
@@ -0,0 +1,10 @@
+let num1 = prompt("digite um numero");
+let num2 = prompt("digite outro numero");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+let soma = num1 + num2;
+alert ("O resultado é: " +soma);
+
+
